dbe140fa0b9ea181944d672af1a45fd7668d6e62
Stefan Marks <stefan.marks.ac@gmail.com>
2016-01-06 14:27:44 +1300

    Added documentation to MoCap interface

--- src/MoCapSystem.h
+++ src/MoCapSystem.h
@@ -10,16 +10,73 @@
 // these two functions can be used by the class to stream frames
 extern void signalNewFrame();
 
+/**
+ * Pure virtual base class for the minimum MoCap system methods.
+ */
 
 class MoCapSystem
 {
 public:
+
+	/**
+	 * Initialises the MoCap system.
+	 *
+	 * @return <code>true</code> if the initialisation was successful
+	 */
 	virtual bool initialise() = 0;
+
+	/**
+	 * Checks if the MoCap system is active.
+	 *
+	 * @return <code>true</code> if the MoCap system is active
+	 */
 	virtual bool isActive() = 0;
+
+	/**
+	 * Gets the minimum update interval for the system.
+	 *
+	 * @return the minimum update interval for the MoCap system in milliseconds
+	 */
 	virtual int  getUpdateInterval() = 0;
+
+	/**
+	 * Forces an update of the MoCap data.
+	 *
+	 * @return <code>true</code> if the update was successful
+	 */
 	virtual bool update() = 0;
+
+	/**
+	 * Gets a scene description for the NatNet data structure.
+	 *
+	 * @param refData  reference to the data structure to fill in
+	 *
+	 * @return <code>true</code> when the method completed successfully
+	 */
 	virtual bool getSceneDescription(MoCapData& refData) = 0;
+
+	/**
+	 * Gets the data for a single frame as a NatNet data structure.
+	 *
+	 * @param refData  reference to the data structure to fill in
+	 *
+	 * @return <code>true</code> when the method completed successfully
+	 */
 	virtual bool getFrameData(MoCapData& refData) = 0;
+
+	/**
+	 * Processes a custom string command.
+	 *
+	 * @param strCommand  the command to execute
+	 *
+	 * @return <code>true</code> when the command was execded successfully
+	 */
 	virtual bool processCommand(const std::string& strCommand) = 0;
+
+	/**
+	 * Deinitialises the MoCap system.
+	 *
+	 * @return <code>true</code> when the system was deinitialised successfully
+	 */
 	virtual bool deinitialise() = 0;
 };
